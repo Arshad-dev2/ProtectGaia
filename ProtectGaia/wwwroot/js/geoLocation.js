d7f4b5abe70553d6140c3f648661b1c02e5e55b9
Bhavna104 <49225057+Bhavna104@users.noreply.github.com>
2020-05-09 16:59:37 +1000

    blogs 09052020

--- ProtectGaia/wwwroot/js/geoLocation.js
+++ ProtectGaia/wwwroot/js/geoLocation.js
@@ -32,12 +32,17 @@ function makeCall(location) {
         cache: false,
         async: false,
         success: function (response) {
-            if (response.isSucess==true) {
+
+
+            if (response.isSucess == true) {
+                var aqi_category = CategoryAqi(response.location.aqi);
+                var ozone_category = CategoryOzone(response.location.ozone);
                 $(".deviceLocation").html("<p><i class=\"fa fa-map-marker\" style=\"color: #ff3333\"></i>&nbsp;" + response.location.address + "</p>");
-                $(".currentTemp").html("<h3>Current Temperature</h3><p style=\"font-size:1.5em;\">" + response.location.temp + "&#8451" + "<b>/</b>"  + response.location.feelsLikeTemp + "&#8451;</p>");
-                $(".AqiValue").html("<h3>Air Quality Index</h3><p style=\"font-size:1.5em;\"> " + response.location.aqi + "&nbsp;</h1 >μg/m<superscript>3</superscript>");
-                $(".OzoneValue").html("<h3>Ozone Index</h3><p style=\"font-size:1.5em;\"> " + response.location.ozone + "&nbsp;</h1 >μg/m<superscript>3</superscript>");
+                $(".currentTemp").html("<h3>Current Temperature</h3><p style=\"font-size:1.5em;\">" + response.location.temp + "&#8451" + "<b>/</b>" + response.location.feelsLikeTemp + "&#8451;</p>");
+                $(".AqiValue").html("<h3>Air Quality Index</h3><p style=\"font-size:1.5em;\"> " + response.location.aqi + "&nbsp;</h1 >μg/m<superscript>3</superscript><p style=\"color:black\"><b>" + aqi_category + "</b></p>");
+                $(".OzoneValue").html("<h3>Ozone Index</h3><p style=\"font-size:1.5em;\"> " + response.location.ozone + "&nbsp;</h1 >ppb<p style=\"color:black;\"> <b>" + ozone_category + "</b></p>");
             }
+
             else {
                 $(".deviceLocation").html("<br />Your device location<br/> <b>" + "Location Unavailable" + "</b>");
                 $(".currentTemp").html("<br /><b>Current Weather</b><br/>Unavailable");
@@ -48,4 +53,33 @@ function makeCall(location) {
         
     })
 
+}
+
+function CategoryAqi(value) {
+    if (value >= 0 && value <= 50) {
+        return "Good and Healthy";
+    }
+    else if (value > 50 && value <= 100) {
+        return "Moderate and Healthy";
+    }
+    else if (value > 100 && value <= 150) {
+        return "Unhealthy to Sensitivite groups";
+    }
+    else {
+        return "Bad and Unhealthy";
+    }
+}
+function CategoryOzone(value) {
+    if (value >= 0 && value <= 59) {
+        return "Good and Healthy";
+    }
+    else if (value > 60 && value <= 75) {
+        return "Moderate and Healthy";
+    }
+    else if (value > 76 && value <= 95) {
+        return "Unhealthy to Sensitivite groups";
+    }
+    else {
+        return "Bad and Unhealthy";
+    }
 }
